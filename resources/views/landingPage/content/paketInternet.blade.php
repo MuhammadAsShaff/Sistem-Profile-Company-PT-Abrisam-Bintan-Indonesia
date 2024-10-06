6d28a394b577d159aacc8d2e1d1d08abf05f859f
Muhammad As Shaff <as22si@mahasiswa.pcr.ac.id>
2024-10-06 22:49:56 +0700

    Atur Dikit Dikit Tampilan Paket Internet

--- resources/views/landingPage/content/paketInternet.blade.php
+++ resources/views/landingPage/content/paketInternet.blade.php
@@ -36,7 +36,7 @@
   </div>
 
   <!-- Cards Section -->
-  <div id="produk-container" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+  <div id="produk-container" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 py-6">
     @foreach($produk as $prod)
     <div class="max-w-sm bg-white shadow-2xl shadow-gray-400 rounded-lg p-6 relative">
       <!-- Promo Badge -->
