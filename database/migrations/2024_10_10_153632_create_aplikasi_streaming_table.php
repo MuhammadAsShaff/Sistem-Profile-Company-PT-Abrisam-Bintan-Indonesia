9ea3bdffd24d13e942f5687c4509d3dec0545944
Muhammad As Shaff <as22si@mahasiswa.pcr.ac.id>
2024-10-10 16:13:28 +0700

    Setup Aplikasi Streaming

--- database/migrations/2024_10_10_153632_create_aplikasi_streaming_table.php
+++ database/migrations/2024_10_10_153632_create_aplikasi_streaming_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('aplikasi_streaming', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_aplikasi'); // Primary key as 'id_aplikasi'
+            $table->string('nama_aplikasi'); // Column for 'nama_aplikasi'
             $table->timestamps();
         });
     }
